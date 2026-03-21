b1dd2da782f201409aba1aaaff01bdf2eb5ef2e7
Layo-Dev <benedictchinaza765@gmail.com>
2026-03-21 16:51:59 +0100

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,4 @@
-import styles from "./Hero.module.css";
+import styles from "./hero.module.css";
 import herobg2 from "../../assets/images/herobg2.png";
 import { useState } from "react";
 import SnapModal from "../snapmodal/SnapModal";
